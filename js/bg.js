5471a83344bd189f9a5d0d00769c3b2821e092e1
cinap <wjsdur0824@gmail.com>
2021-04-30 17:54:19 +0900

    Change IMG_NUMBER

--- js/bg.js
+++ js/bg.js
@@ -1,21 +1,20 @@
 const body = document.querySelector("body");
 
-const IMG_NUMBER = 3;
+const IMG_NUMBER = 5;
 
-function paintImage(imgNumber){
-    const image = new Image();
-    image.src=`../images/${imgNumber +1}.jpg`;
-    image.classList.add('bgImage');
-    body.appendChild(image);
+function paintImage(imgNumber) {
+  const image = new Image();
+  image.src = `../images/${imgNumber + 1}.jpg`;
+  image.classList.add("bgImage");
+  body.appendChild(image);
+}
+function genRandom() {
+  return (number = Math.floor(Math.random() * IMG_NUMBER));
 }
 
-function genRandom(){
-    return number = Math.floor(Math.random() * IMG_NUMBER);
-}
-
-function init(){
-    const randomNumber = genRandom();
-    paintImage(randomNumber);
+function init() {
+  const randomNumber = genRandom();
+  paintImage(randomNumber);
 }
 
 init();
